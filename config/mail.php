656fa287779f18ff08a9737f729a206707bf11ff
hannahgonzaga01 <hannahgonzaga01@gmail.com>
2026-09-14 00:55:10 +0800

    Configure Gmail SMTP fallbacks for live email notifications on Render

--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'log'),
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,12 +41,12 @@ return [
             'transport' => 'smtp',
             'scheme' => env('MAIL_SCHEME'),
             'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+            'port' => env('MAIL_PORT', 587),
+            'username' => env('MAIL_USERNAME', 'brgysanmigueldos@gmail.com'),
+            'password' => env('MAIL_PASSWORD', 'ozpwjurnkyeowwzo'),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'timeout' => null,
+            'timeout' => 15,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
@@ -112,8 +112,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'brgysanmigueldos@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Barangay San Miguel II'),
     ],
 
 ];
